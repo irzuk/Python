457cbf158e009834a33fd5186fc2298d05ca6ca1
Zelentsova Irina <irinazel2002@gmail.com>
2023-03-23 23:22:49 +0300

    aaa

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 # syntax=docker/dockerfile:1
 
 FROM python:3.9
-COPY requirements.txt requirements.txt
-
-RUN pip3 install -r requirements.txt
-
 COPY . .
-
-pdflatex artifacts/table.tex
+RUN apt-get update && apt-get -y upgrade
+RUN pip install pdflatex
+RUN apt-get install -y texlive graphviz graphviz-dev python3-pygraphviz
+RUN pip install networkx==2.8
+RUN pip install pygraphviz
+RUN pip install -i https://test.pypi.org/simple/ testpackagefibbfunpicturegen==0.1.6
+RUN python3 main.py
+RUN pdflatex artifacts/medium_task.tex
--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 # import as pic_gen
 
+from functools import reduce
+from testpackagefibbfunpicturegen import generate
 def convert_arr_to_str(arr):
-    return "\documentclass{article}\n\\begin{document}\n\\begin{center}\n\\end{document}\n"
+    ans = '\\begin{tabular}{|'
+    s = len(arr[0])
+    ans += 'c|' * s + '}\n\\hline\n'
+    # reduce
+    ans += reduce(
+        lambda l, r: l + '\\hline\n' + r,
+        map(lambda line: reduce(lambda l, r: str(l) + ' & ' + str(r), line) + ' \\\\\n', arr)
+    )
+    ans += '\\hline\n\\end{tabular}\n'
+    return ans
 
 
 def easy_task(arr):
+    head = '\\documentclass{article}\n\\usepackage[utf8]{inputenc}\n\\usepackage[' \
+           'english]{babel}\n\\usepackage{graphicx}\n\\graphicspath{ {./artifacts/} }\n\\begin{document}\n'
+    end = '\\end{document}\n'
     f = open("artifacts/easy_task.tex", "w")
-    f.write(convert_arr_to_str(arr))
+    f.write(head + convert_arr_to_str(arr) + end)
     f.close()
 
 
 def medium_task(arr):
     easy_task(arr)
-    # pic_gen.main()
+    generate()
     with open("artifacts/easy_task.tex", "r") as old_tex:
         contents = old_tex.readlines()
 
     contents.insert(1, "\\usepackage{graphicx}\n")
-    contents.insert(len(contents) - 1, "\includegraphics{artifacts/ast.png}\n")
+    contents.insert(len(contents) - 1, "\n\n\includegraphics[scale=0.2]{artifacts/ast.png}\n")
     new_tex = open("artifacts/medium_task.tex", "w")
     new_tex.writelines(contents)
 
 
 def main():
     arr = [["res: " + str(i + j) for j in range(8)] for i in range(7)]
+    easy_task(arr)
     medium_task(arr)
 
 
